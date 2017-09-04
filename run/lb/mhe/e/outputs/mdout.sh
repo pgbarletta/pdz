537998510c25e25e2eea35fd28e52442f6ecb44b
pgbarletta <pbarletta@gmail.com>
2017-09-04 13:01:16 -0400

    Done w/ mhe analysis

--- run/lb/mhe/e/outputs/mdout.sh
+++ run/lb/mhe/e/outputs/mdout.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Extract heat and equil mdout info
-perl process_mdout.perl 1equ.out 2equ.out 3equ.out 4equ.out 5equ.out 6equ.out 7equ.out 8equ.out 9equ.out 10equ.out 11equ.out 12equ.out 13equ.out 14equ.out 15equ.out 16equ.out 17equ.out 18equ.out 19equ.out 20equ.out 21equ.out 22equ.out 23equ.out 24equ.out 25equ.out 26equ.out 27equ.out 28equ.out 29equ.out 30equ.out 31equ.out 32equ.out 33equ.out 34equ.out 35equ.out
-
+perl process_mdout.perl 1equ.out 2equ.out 3equ.out 4equ.out 5equ.out 6equ.out 7equ.out 8equ.out 9equ.out 10equ.out 11equ.out 12equ.out 13equ.out 14equ.out 15equ.out 16equ.out 17equ.out 18equ.out 19equ.out 20equ.out 21equ.out 22equ.out 23equ.out 24equ.out 25equ.out 26equ.out 27equ.out 28equ.out 29equ.out 30equ.out 
 
 mv summary.ETOT etot.dat
 mv summary.EPTOT eptot.dat
